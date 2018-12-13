644cea3f24e5a8291fbe5c68876f9b4463230011
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2018-12-13 15:38:27 +0100

    new filenames

--- Abon/animation_figures/rename_png.sh
+++ Abon/animation_figures/rename_png.sh
@@ -2,9 +2,24 @@
 
 # the name following the 7digit number in the filename of the png files in the folder  (without .png)
 
-mv col711.png col0000711.png
+i=2
+
+while [ $i -lt 766 ]; do
+    
+    if [ $i -lt 10 ]; then
+        mv col${i}.png col00000${i}.png
+
+    elif [ $i -gt 9 ] && [ $i -lt 100 ]; then
+		mv col${i}.png col0000${i}.png
+
+	elif [ $i -gt 99 ]; then
+		mv col${i}.png col000${i}.png
+    fi
+
+    i=$((i+1))
+
+done
 
 
 
-
-
+#col000765.png
