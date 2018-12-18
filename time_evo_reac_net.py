008a0c53b7936c5104a742a63181880392b22557
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2018-12-18 12:47:25 +0100

    new file to plot (clean version)

---
+++ time_evo_reac_net.py
@@ -0,0 +1,23 @@
+# This script plots the physical quantities of the Rpro output at 
+# a given time or evolves the time (animation)
+
+# The output files of the Rpro calc is copied over to the directory 
+# /Abon
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+ID_merger = "0132511"
+
+file_aa = "aa" + ID_merger
+file_pf = "pf_" + file_aa
+
+print file_pf
+
+
+
+
+
+
