e6031938d7b9a4b449ee02ac061cc020cb2f7ec3
Ina K. Berentsen Kullmann <ikkullma@student.matnat.uio.no>
2018-12-18 17:20:17 +0100

    ready to make plots

--- visualize_output_pf.py
+++ visualize_output_pf.py
@@ -1,4 +1,4 @@
-# This script plots the physical quantities of the Rpro output at 
+# This script plots the physical quantities of the Rpro output (pf file) at 
 # a given time or evolves the time (animation)
 
 # The output files of the Rpro calc has to be copied over to the directory 
@@ -12,5 +12,42 @@ import os
 ID_merger = "0132511"
 
 directory = "Abon/"
-file_aa = directory + "aa" + ID_merger
-file_pf = directory + "pf_" + file_aa
+file_pf = directory + "pf_" + "aa" + ID_merger
+
+# The file is structured as one parameter per column and the time evolves
+# for each row. The parameters are found in the column:
+#parameter_names = np.genfromtxt(file_pf,skip_header=10,max_rows=1,dtype='str')
+#print parameter_names
+#print len(parameter_names) # = 31 parameters
+
+
+# Access the matrix as: data_matrix[time_row][prameter_col]
+data_matrix = np.genfromtxt(file_pf,skip_header=11)
+
+lsod = data_matrix[:,0]
+time = data_matrix[:,1]
+dt   = data_matrix[:,2]
+T9   = data_matrix[:,3]
+rho  = data_matrix[:,4]
+Nn   = data_matrix[:,5]
+Xn   = data_matrix[:,6]
+#..
+Ye   = data_matrix[:,9]
+#..
+Qtot = data_matrix[:,12]
+Qng  = data_matrix[:,13]
+Qgn  = data_matrix[:,14]
+Qplus= data_matrix[:,15]
+Qbeta= data_matrix[:,16]
+Qfis = data_matrix[:,17]
+Qalp = data_matrix[:,18]
+#..
+r    = data_matrix[:,-2]
+v    = data_matrix[:,-1]
+
+plt.loglog(time,Qplus)
+
+plt.show()
+
+
+
